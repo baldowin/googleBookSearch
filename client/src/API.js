51af40ed0caaec3d158c9be6f3d3d61bc2cedeeb
baldowin <jeb89@miami.edu>
2019-05-03 17:05:28 -0500

    starting search

--- client/src/API.js
+++ client/src/API.js
@@ -16,5 +16,8 @@ export default {
   // Saves a book to the database
   saveBook: function(bookData) {
     return axios.post("/api/books", bookData);
+  },
+  google: function() {
+    return axios.get("/api/google");
   }
 };
--- routes/google.js
+++ routes/google.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
 const router = require("express").Router();
+const apiKey = require(".env");
 
 router.get("/", (req, res) => {
   axios
-    .get("", { params: req.query })
+    .get("https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key="+apiKey, { params: req.query })
     .then(({ data: { results } }) => res.json(results))
     .catch(err => res.status(422).json(err));
 });
